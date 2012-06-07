06e69535a3eab0a0388a9043d1a886d6bfa44182
Joao Soares <joaofelipe1395@gmail.com>
2012-06-07 19:50:36 -0300

    lost finished unit 4 due to git dumbness
    but recovered some parts, missing forms I took so long to make... i
    think i'm gonna cry

--- unit4/main.py
+++ unit4/main.py
@@ -17,6 +17,24 @@
 import os
 import webapp2
 import jinja2
+import hmac
+import re
+
+SECRET = "imsosecret"
+
+def hash_str(s):
+    ''' Return the MD5-hash of a string'''
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    ''' Takes a string and returns a string of the format s,HASH'''
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):    
+    ''' Takes string of format s,HASH and returns s if hash_str(s) == HASH, otherwise None'''
+    s = h[:h.rfind("|")] # finds last comma with .rfind() before HASH
+    if make_secure_val(s) == h:
+        return s
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -31,19 +49,88 @@ class Handler(webapp2.RequestHandler):
         return t.render()
     # takes both functions above and joins them
     def render(self, template, **kw):
-        self.write(render_str(template, **kw))
+        self.write(self.render_str(template, **kw))
 
 class MainHandler(Handler):
     def get(self):
-    	self.response,headers['Content-Type'] = 'text/plain'
-    	visits = self.request.cookies.get('visits', 0)
-    	visits += 1
-    	self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+    	self.response.headers['Content-Type'] = 'text/plain'
+    	# creates a temporary variable for visits
+        visits = 0
+        
+        # tries to get cookie with number of previous visits
+        visit_cookie_str = self.request.cookies.get('visits')
+        # if page has been visited before add number of previous visits to temp var.
+        # if not, nothing will be added and the var will be 0, as expected
+    	if visit_cookie_str: # checks if user has been to page before
+            # checks return hashed val to see if cookies were changed
+            cookie_val = check_secure_val(visit_cookie_str)
+            # if they weren't, use them. else, start over form 0
+            if cookie_val:
+                visits = int(cookie_val)
 
-    	self.write("You have been here %s times!" % visits)
+        #increment no of visits by 1 - that's the current visit
+    	visits +=1
 
+        # pass on the new value of visits to a cookie header to be served next time page is visited
+        new_cookie_val = make_secure_val(str(visits))
+
+    	self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+
+    	if visits > 10000:
+    		self.write("You are so cool dude")
+    	else:
+	    	self.write("You have been here %s times!" % visits)
+
+class SignupHandler(Handler):
+    def get(self):
+        self.render("signup.html")
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+
+        errors = {}
+        check_errors(errors, username, password, verify, email)
+        if len(errors) == 0:
+            pass
+        else:
+            errors['username'] = username
+            errors['email'] = email
+            self.render("signup.html", **errors)
+
+        
 
 app = webapp2.WSGIApplication([
-	('/', MainHandler)
-
+	('/', MainHandler),
+    ('/signup', SignupHandler),
 	], debug=True)
+
+
+
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASS_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+def check_errors(params, username, password, verify, email):
+    if not valid_username(username):
+        params['username_error'] = "Missing or invalid username."
+    if not valid_password(password):
+        params['password_error'] = "Missing or invalid password."
+    if password != verify:
+        params['verify_error'] = "Passwords don't match."
+    if not valid_email(email):
+        params['email_error'] = "There is an error with your email."
+
